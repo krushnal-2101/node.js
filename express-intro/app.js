daf9879126d4e32d5e830a4e2e9a2305103b800a
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2026-02-04 11:47:52 +0530

    Add files via upload

---
+++ express-intro/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+
+const app = express();
+
+
+app.get("/", (req, res)=> [
+    res.json("hello express server")
+])
+
+app.get("/about", (req, res) => {
+    res.json("about page")
+} )
+
+const port = 5000;  
+
+
+app.listen(port, (err)=> {
+    if(err){
+        console.log(err)
+
+    }
+    console.log("server running port ", port)
+})
