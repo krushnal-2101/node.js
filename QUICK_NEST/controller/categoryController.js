0571304d730aaef6b45bd8f1bbfe563607f7e45c
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2026-04-13 13:01:43 +0530

    Add files via upload

--- QUICK_NEST/controller/categoryController.js
+++ QUICK_NEST/controller/categoryController.js
@@ -64,5 +64,77 @@ const getById = async (req, res, next) => {
 };
 
 
+const update = async (req, res, next) => {
+  try {
+    const { id } = req.params;
 
-export default { add };
+    let category = await Category.findById(id);
+
+    if (!category) {
+      return next(new HttpError("category not found", 404));
+    }
+
+  
+    if (
+      req.user.role !== "admin" &&
+      req.user.role !== "super_admin"
+    ) {
+      return next(new HttpError("unauthorized access", 401));
+    }
+
+    const updates = Object.keys(req.body);
+    const allowedFields = ["name", "description"];
+
+    const isValid = updates.every((field) => allowedFields.includes(field));
+
+    if (!isValid) {
+      return next(new HttpError("only allowed field can be updated", 400));
+    }
+
+  
+    if (req.body.name && req.body.name !== category.name) {
+      const existingCategory = await Category.findOne({ name: req.body.name });
+      if (existingCategory) {
+        return next(new HttpError("category name already exists", 400));
+      }
+    }
+
+    updates.forEach((update) => (category[update] = req.body[update]));
+
+    await category.save();
+
+    res.status(200).json({
+      success: true,
+      message: "category updated successfully",
+      category,
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+
+
+const deleteCategory = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const category = await Category.findById(id);
+
+    if (!category) {
+      return next(new HttpError("category not found", 404));
+    }
+
+    await Category.findByIdAndDelete(id);
+
+    res.status(200).json({
+      success: true,
+      message: "category deleted successfully",
+    });
+  } catch (error) {
+    next(new HttpError(error.message, 500));
+  }
+};
+
+
+export default { add, getAll, getById, update, deleteCategory };
