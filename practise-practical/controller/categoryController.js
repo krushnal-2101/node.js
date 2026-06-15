5651e775747b707fabd7fb0028085f0ea1b6618a
Krushnal Bhatti <krushnalbhatti71@gmail.com>
2026-06-15 11:42:34 +0530

    Add files via upload

--- practise-practical/controller/categoryController.js
+++ practise-practical/controller/categoryController.js
@@ -1,11 +1,40 @@
-import Category  from "../models/categoryModel.js"
+import Category from '../models/Category.js';
 
-const createcategory =  async (req, res) => {
-    try{
-        const { name} = req.body;
-        const cat = await Category.create({ name })
-        res.status(201).json(cat);
-    }catch(err) {
-        res.status(500).json({ message: err.message})
-    }
-}
+ const createCategory = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const cat = await Category.create({ name });
+    res.status(201).json(cat);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+ const getCategories = async (req, res) => {
+  try {
+    const cats = await Category.find();
+    res.json(cats);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+ const updateCategory = async (req, res) => {
+  try {
+    const cat = await Category.findByIdAndUpdate(req.params.id, { name: req.body.name }, { new: true });
+    res.json(cat);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+const deleteCategory = async (req, res) => {
+  try {
+    await Category.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Deleted' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+
+export { createCategory, getCategories, updateCategory, deleteCategory };   
